c4282ad112a9c4f5866c2db9cc7f04bbbc7abc11
Steve Wayne <stevewayne478@gmail.com>
2025-08-13 19:54:39 +0530

    fix_lusca

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import lusca from "lusca";
 dotenv.config();
 
 const app = express();
@@ -18,7 +18,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(require('lusca').csrf());
+
 
 // Routes
 import userRouter from "./routes/user.routes.js";
